63cc4ec54e5808eeb9846c2919511c9890192234
joestarhu <56381125+joestarhu@users.noreply.github.com>
2020-05-18 23:21:54 +0800

    追加缓冲设定时间为1s

--- tklc可视化界面/main.py
+++ tklc可视化界面/main.py
@@ -5,7 +5,7 @@ from flask_script import Manager
 from tklc import tklc_init
 
 app = tklc_init(os.getenv('FLASK_ENV') or 'default')
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 manager = Manager(app)
 
 if __name__ == "__main__":
