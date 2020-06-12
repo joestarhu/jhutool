bd1b2fd3766227fe3bfc543e90810b9a3b0708f7
joestarhu <56381125+joestarhu@users.noreply.github.com>
2020-06-12 16:42:47 +0800

    Update main.py

--- tklc项目管理/main.py
+++ tklc项目管理/main.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python3
-"""
-自动创建禅道任务
-"""
 from pjinfo import PJinfo
-from zendao import Zendao
+from zendao import ZenTao
 
 def create_task(xlsx_id,module_id=None):
     p = PJinfo()
@@ -20,11 +17,12 @@ def create_task(xlsx_id,module_id=None):
     title = pj["内容范围简介"].values[0]
     task_name = f'#{prod}{ver}# {title}'
 
-    z = Zendao()
-    z.task_cteate(bus,module_id,task_name,member)
+
+    z = ZenTao('***','****')
+    z.task(bus,module_id,task_name,member)
 
 
 if __name__ == '__main__':
-    Excel_id = 13
-    Moudle_id = 1227
-    # create_task(Excel_id,Moudle_id)
+    Excel_id = 11
+    Moudle_id = 1239
+    #create_task(Excel_id,Moudle_id)
